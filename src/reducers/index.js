b4df5845436a70ffb9032fc775e0522abc73146f
Joel <wilddroppings@gmail.com>
2021-04-12 17:28:45 -0600

    removed unused code

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,9 @@
-const initialState = { 
-  dadJoke:[]
-};
+const initialState = {};
 
 const reducer = (state = initialState, action) => {
-    console.log(action)
     if(action.type === 'DATA_LOADED') {
         return {dadJoke: action.payload.joke, id: action.payload.id}
     }
-    console.log('state', state)
     return state;
 }
 
