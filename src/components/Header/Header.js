9afddf2feb7dffef202d89bc305013fe8a036ba4
jgiwinski <juliaiwinski@gmail.com>
2021-04-12 20:22:21 -0700

    Style header and add buttons

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header>
+            <div className='title-container'>
+                <h4 className="site-title">My Name is Dad</h4>
+                <p>Hi, my name is App.</p>
+                <p>Hi App, my name is Dad!</p>
+            </div>
+            <div className="nav-btn-container">
+                <button className="nav-btn">Be Your Own Dad</button>
+                <button className="nav-btn">Favorites</button>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
