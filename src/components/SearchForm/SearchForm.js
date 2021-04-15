e78389665be7dfc09fb2c852ff53fc08f0c10f1c
Joel <wilddroppings@gmail.com>
2021-04-15 12:27:18 -0600

    Remove class status from searched dad jokes component

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { searchData } from '../../actions/index';
 import { connect } from 'react-redux';
-import { Link } from 'react-router';
 
 class SearchForm extends Component {
   constructor() {
