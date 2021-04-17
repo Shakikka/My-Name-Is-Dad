1332f17cce72b6676038fea65fe341b1df8bbcb3
jgiwinski <juliaiwinski@gmail.com>
2021-04-17 16:18:31 -0700

    Add conditional rendering to buttons for each page view

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import './Header.css';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Header = () => {
+
+    const location = useLocation().pathname; 
+    let abode = true; 
+    let search = true; 
+    if(location === '/') {
+        abode = true;
+        search = false; 
+    } else if (location === '/search' ) {
+        abode = false; 
+        search = true; 
+    }
+
     return (
         <header>
             <div className='title-container'>
@@ -12,10 +24,10 @@ const Header = () => {
             </div>
             <div className="nav-btn-container">
                 <Link to={'/'}>
-                    <button className="nav-btn">Humble Abode</button>
+                     {!abode && <button className="nav-btn">Humble Abode</button>}
                 </Link>
                 <Link to={'/search'}>
-                    <button className="nav-btn">Search</button>
+                    {!search && <button className="nav-btn">Search</button>}
                 </Link>
             </div>
         </header>
