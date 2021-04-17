be39bc95fbf0a00f4aaeb873fb26198d4cea4239
jgiwinski <juliaiwinski@gmail.com>
2021-04-17 14:44:13 -0700

    Add animation to nav buttons

--- src/components/SearchedDadJokes/SearchedDadJokes.js
+++ src/components/SearchedDadJokes/SearchedDadJokes.js
@@ -6,9 +6,7 @@ const SearchedDadJokes = (props) => {
     <ul >
       {props.filteredJokes && props.filteredJokes.map(joke => {
         return (
-          <div className='searched-container'>
-              <h2 className='slide-top'><li key={joke.id}>{joke.joke}</li></h2>
-        </div>
+            <h2 className='slide-top'><li key={joke.id}>{joke.joke}</li></h2>
         )
       })}
     </ul>
