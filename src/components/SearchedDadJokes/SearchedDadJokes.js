428ac7442ee0e6d1aa8dd34839b81c8f5a42ab7c
Joel <wilddroppings@gmail.com>
2021-04-18 13:20:08 -0600

    Change text on no returned search results

--- src/components/SearchedDadJokes/SearchedDadJokes.js
+++ src/components/SearchedDadJokes/SearchedDadJokes.js
@@ -5,7 +5,7 @@ const SearchedDadJokes = (props) => {
   return (
     <ul >
       {props.filteredJokes && !props.filteredJokes.search_term && <h2>Please insert a Dadegory above...</h2>}
-      {props.filteredJokes && !props.filteredJokes.results.length && <h2>Sorry, no Dadegory matched your search...</h2>}
+      {props.filteredJokes && !props.filteredJokes.results.length && <h2>I'm Sorry, but your Dadegory isn't Dadly enough...</h2>}
       {props.filteredJokes && props.filteredJokes.search_term && props.filteredJokes.results.map(joke => {
         return (
             <h2 className='slide-top' key={joke.id}><li>{joke.joke}</li></h2>
